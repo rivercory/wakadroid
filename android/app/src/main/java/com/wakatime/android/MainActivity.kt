5407d02b8881d2c96f556c4efeac2b96710a3d52
Hyeonho Kang <hyeonhokang10@kakao.com>
2024-07-21 20:36:07 +0900

    Add android native app

---
+++ android/app/src/main/java/com/wakatime/android/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.wakatime.android
+
+import android.app.DownloadManager
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.webkit.DownloadListener
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://wakatime.com")
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportZoom(true)
+        webView.settings.domStorageEnabled = true
+        webView.setDownloadListener(DownloadListener { url, userAgent, contentDisposition, mimetype, contentLength ->
+            val request = DownloadManager.Request(
+                Uri.parse(url)
+            )
+            request.allowScanningByMediaScanner()
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED) //Notify client once download is completed!
+            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            dm.enqueue(request)
+            Toast.makeText(
+                applicationContext,
+                "Downloading file",
+                Toast.LENGTH_LONG
+            ).show()
+        })
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack())
+            webView.goBack()
+        else
+            super.onBackPressed()
+    }
+}
